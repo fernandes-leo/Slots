3cb4873e942ee0d685d1f8818c229d10fc1e709f
Leonardo <fernandes.leo@live.com>
2022-01-27 09:16:00 -0300

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Leonardo Fernandes on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
